41db3db33a240e7a8432d0d026162ad53ae2751a
Stiven <stivencastillo.90@gmail.com>
2017-03-10 06:14:16 +0000

    Autenticacion laravel, Migraciones

---
+++ database/migrations/2017_03_09_170249_create_product_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('categories_translations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->string('locale')->index();
+
+            $table->string('name');
+
+            $table->unique(['category_id','locale']);
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+
+        Schema::create('products_translations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->string('locale')->index();
+
+            $table->string('name');
+            $table->text('description');
+
+            $table->unique(['product_id','locale']);
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_translations');
+        Schema::dropIfExists('categories_translations');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
+    }
+}
